07627d1d56a5339ca106d09893db898b8d1df053
LucasVB2729 <buenolucas259@gmail.com>
2024-04-04 14:35:16 -0300

    Exercício Final RIC

---
+++ Exs_Aula8/ex13_RIC.py
@@ -0,0 +1,23 @@
+kwh_consumida = float(input("\nDigite a quantidade de kWh consumida: "))
+tipo_instalacao = input("Digite o tipo de instalação (R para residências, I para indústrias, C para comércios): ")
+
+if tipo_instalacao.upper() == 'R':  # Residencial
+    if kwh_consumida <= 500:
+        preco_pagar = kwh_consumida * 0.40
+    else:
+        preco_pagar = kwh_consumida * 0.65
+elif tipo_instalacao.upper() == 'C':  # Comercial
+    if kwh_consumida <= 1000:
+        preco_pagar = kwh_consumida * 0.55
+    else:
+        preco_pagar = kwh_consumida * 0.60
+elif tipo_instalacao.upper() == 'I':  # Industrial
+    if kwh_consumida <= 5000:
+        preco_pagar = kwh_consumida * 0.55
+    else:
+        preco_pagar = kwh_consumida * 0.60
+else:
+    print("Tipo de instalação inválido!\n")
+    exit()
+
+print(f"O preço a pagar pelo fornecimento de energia elétrica é: R$ {preco_pagar}\n")
